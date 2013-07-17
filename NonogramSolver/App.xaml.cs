f64645af83be2d67a06d7ef5ab91ff14d527ec6b
Lipotam <Lipotam@gmail.com>
2013-07-17 23:01:00 +0300

    project creating

---
+++ NonogramSolver/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace NonogramSolver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
