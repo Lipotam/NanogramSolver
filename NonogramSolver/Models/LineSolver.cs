9fa7e157b10422b50bdf2799650324d30e54d0da
Lipotam <Lipotam@gmail.com>
2013-09-20 20:12:39 +0300

    some interfaces added

---
+++ NonogramSolver/Models/LineSolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NonogramSolver.Models
+{
+    public class LineSolver
+    {
+        public LineSolver(CellState[] linevector, List<int> numbers)
+        {
+            this.lineVector = linevector;
+            this.numbers = numbers;
+        }
+
+        private CellState[] lineVector;
+        private List<int> numbers;
+
+        public void UpdateLineVector(CellState[] linevector)
+        {
+            this.lineVector = linevector;
+        }
+
+        public CellState[] GetLineVector()
+        {
+            return lineVector;
+        }
+
+        public void RecogniseLine()
+        {
+
+
+
+
+        }
+    }
+}
