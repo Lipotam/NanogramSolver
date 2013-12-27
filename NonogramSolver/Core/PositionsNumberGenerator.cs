7491e4d28e3395a5ca39777b3abc9d4976135342
Lipotam <Lipotam@gmail.com>
2013-12-27 16:49:50 +0300

    positions number generator added

---
+++ NonogramSolver/Core/PositionsNumberGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace NonogramSolver.Core
+{
+    class PositionsNumberGenerator
+    {
+        public BigInteger PositionNumber(int count, int summ, int width)
+        {
+            if (count == 1)
+            {
+                return width - summ - count + 2;
+            }
+            else
+            {
+                BigInteger result = 0;
+
+                for (int i = 1; i < width - summ - count + 3; i++)
+                {
+                    result += PositionNumber(count-1, summ, width-i);
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ NonogramSolver/Tests/PositionsNumberGeneratorTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+using NonogramSolver.Core;
+
+namespace NonogramSolver.Tests
+{
+    [TestFixture]
+    class PositionsNumberGeneratorTests
+    {
+        private PositionsNumberGenerator target;
+
+        [SetUp]
+        public void Init()
+        {
+            target = new PositionsNumberGenerator();
+        }
+
+        [Test]
+        public void FirstPowerTest()
+        {
+            Assert.AreEqual((BigInteger)2, target.PositionNumber(1, 1, 2));
+            Assert.AreEqual((BigInteger)1, target.PositionNumber(1, 1, 1));
+            Assert.AreEqual((BigInteger)5, target.PositionNumber(1, 2, 6));
+        }
+
+        [Test]
+        public void SecondPowerTest()
+        {
+            Assert.AreEqual((BigInteger)1, target.PositionNumber(2, 4, 5));
+            Assert.AreEqual((BigInteger)6, target.PositionNumber(2, 7, 10));
+        }
+
+        [Test]
+        public void ThirdPowerTest()
+        {
+            Assert.AreEqual((BigInteger)35, target.PositionNumber(3, 3, 9));
+            Assert.AreEqual((BigInteger)56, target.PositionNumber(3, 3, 10));
+        }
+
+        [Test]
+        public void FourthPowerTest()
+        {
+            Assert.AreEqual((BigInteger)15, target.PositionNumber(4, 5, 10));
+            Assert.AreEqual((BigInteger)5, target.PositionNumber(4, 5, 9));
+        }
+    }
+}
